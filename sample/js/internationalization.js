444a91673636555d2fbad339b3618b8b82e34cad
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-03-13 19:04:03 +0100

    Working!

---
+++ sample/js/internationalization.js
@@ -0,0 +1,16 @@
+function translatePage(path) 
+{
+    var userLang = navigator.language || navigator.userLanguage;
+    if (userLang.indexOf('-') > -1) {
+        userLang = userLang.substr(0, userLang.indexOf(','));
+    }
+    if (path.slice(-1)!=='/')
+        path = path + "/";
+    $.getJSON(path+userLang+'.json')
+        .done(function (data, textStatus, jqXHR) { 
+            $('[data-translatekey]').each(function() {
+                var element = $(this);
+                element.text(data[element.data("translatekey")]);
+            });
+        });
+}; 
