a7a3f64ad3fa7b20b21097794c01e77371b53a1f
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-03-15 08:56:46 +0100

    Library file renamed

--- sample/js/simple_i18n.js
+++ sample/js/simple_i18n.js
@@ -7,7 +7,7 @@ function translatePage(path)
     if (path.slice(-1)!=='/')
         path = path + "/";
     $.getJSON(path+userLang+'.json')
-        .done(function (data, textStatus, jqXHR) { 
+        .done(function (data, textStatus, jqXHR) {
             $('[data-translatekey]').each(function() {
                 var element = $(this);
                 element.text(data[element.data("translatekey")]);
